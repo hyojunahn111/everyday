ebb400fc983659abe07b8e22ee110eae726333d4
hyojunahn111 <hyojunahn111@naver.com>
2022-09-27 21:59:49 +0900

    1

---
+++ app/src/main/java/com/example/cocktailapp1_2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cocktailapp1_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler hand = new Handler();
+
+        hand.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
+
+
