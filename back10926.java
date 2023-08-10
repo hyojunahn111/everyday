69d0b0b0633694687a4db46816d9b247c5c59c50
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-08-10 16:42:31 +0900

    백준10926

---
+++ back10926.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class back10926 {
+
+	public static void main(String[] args) {
+		
+		String name;
+		Scanner sc = new Scanner(System.in);
+		String last = "??!";
+		
+		name = sc.next();
+		
+		System.out.println(name + last);
+	}
+
+}
