efabf477dc3ded9abf5d6e0ca8dfb44616711841
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-08-11 12:47:21 +0900

    백준 11382

---
+++ C++/back11382.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int back11382() {
+
+	long long a, b, c = 0;
+
+	cin >> a >> b >> c;
+
+	long long sum = a + b + c;
+
+	cout << sum;
+
+	return 0;
+
+}
---
+++ Java/back11382.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class back11382 {
+
+	public static void main(String[] args) {
+
+		long a, b, c = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		long sum = a + b + c;
+		System.out.println(sum);
+	}
+
+}
