2a2ed6dc2739e91b513ed8b4748082d991550efc
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-08-11 12:20:28 +0900

    백준

---
+++ C++/back18108.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+	int year = 0;
+	cin >> year;
+
+	int dc = 543;
+	year -= dc;
+	cout << year;
+
+	return 0;
+}
---
+++ Java/back18108.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class back18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int year = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+		
+		int dc = 543;
+		
+		System.out.println(year - dc);
+		
+	}
+
+}
