74863ba401cfdd72fe19b777292da2e178db3b48
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-08-14 11:37:44 +0900

    백준 25314

---
+++ Java/back25314.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class back25314 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int longcount = sc.nextInt();
+		int count = longcount / 4;
+		
+		
+		for(int i = 0; i < count; i++) {
+			System.out.print("long ");
+		}
+		System.out.print("int");
+	}
+
+}
