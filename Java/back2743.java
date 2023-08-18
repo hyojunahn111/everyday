f21cd2abc8da86c1b5a86e861e7b1fda1b45905d
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-08-18 16:20:47 +0900

    백준2743

---
+++ Java/back2743.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class back2743 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String word = sc.next();
+		
+		System.out.print(word.length());
+		
+	}
+
+}
