c35c34991428eef416e684f5b63af63ca7b72995
Ahnhyojun <hyojunahn111@naver.com>
2024-08-04 23:10:04 +0900

    기본파일

---
+++ solopj/kakaologinpj/src/main/java/kakaologinpj/KakaologinpjApplication.java
@@ -0,0 +1,13 @@
+package kakaologinpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaologinpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaologinpjApplication.class, args);
+    }
+
+}
---
+++ solopj/kakaologinpj/src/test/java/kakaologinpj/KakaologinpjApplicationTests.java
@@ -0,0 +1,13 @@
+package kakaologinpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaologinpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
